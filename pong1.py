f7204bb45c9b03e5367789d6721280f096c4a860
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-01-04 15:09:07 +0000

    ball can colide with the right paddle;

--- pong1.py
+++ pong1.py
@@ -127,4 +127,12 @@ while True:
       ball.dx *= -1
 
 
+    # paddle and ball colisions
+
+    # right paddle 
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+
     
